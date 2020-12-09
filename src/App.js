18b749a146b772337e2793aec40521047ebeb4e1
gonzser <gonzser@gmail.com>
2020-12-09 02:58:07 -0300

    Con Compose up todo funcionando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit ahora si JUNA!!! <code>src/App.js</code> and save to reload.
+          Edit Maver!!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
